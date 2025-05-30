22281f26574c115d1f25b7be03de52f323c95b19
adsurkasur <superyusuf55@gmail.com>
2025-05-30 19:25:02 +0700

    Update carousel images and enhance card styling for better presentation

--- index.php
+++ index.php
@@ -53,16 +53,16 @@
         </div>
         <div class="carousel-inner">
           <div class="carousel-item active">
-            <img src="REPLACETHISTEXT.jpg" class="d-block w-100" alt="...">
+            <img src="img/01.jpg" class="d-block w-100" alt="...">
           </div>
           <div class="carousel-item">
-            <img src="REPLACETHISTEXT.jpg" class="d-block w-100" alt="...">
+            <img src="img/02.jpg" class="d-block w-100" alt="...">
           </div>
           <div class="carousel-item">
-            <img src="REPLACETHISTEXT.jpg" class="d-block w-100" alt="...">
+            <img src="img/03.jpg" class="d-block w-100" alt="...">
           </div>
           <div class="carousel-item">
-            <img src="REPLACETHISTEXT.jpg" class="d-block w-100" alt="...">
+            <img src="img/04.jpg" class="d-block w-100" alt="...">
           </div>
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
